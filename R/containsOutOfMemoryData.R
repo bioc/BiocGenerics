2272d5e0913ee7cb1acba04e42b7b97f35f5874d
Hervé Pagès <hpages.on.github@gmail.com>
2024-10-01 16:53:42 -0700

    BiocGenerics 0.51.3
    Make containsOutOfMemoryData() work recursively on lists (inspecting each
    list element), environments (inspecting each object in the environment),
    and S4 objects (inspecting each slot).

--- R/containsOutOfMemoryData.R
+++ R/containsOutOfMemoryData.R
@@ -6,7 +6,50 @@ setGeneric("containsOutOfMemoryData",
     function(object) standardGeneric("containsOutOfMemoryData")
 )
 
-setMethod("containsOutOfMemoryData", "ANY", function(object) FALSE)
+.S4_object_contains_out_of_memory_data <- function(object)
+{
+    slot_names <- slotNames(class(object))
+    for (name in slot_names) {
+        if (containsOutOfMemoryData(slot(object, name)))
+            return(TRUE)
+    }
+    FALSE
+}
+
+.list_contains_out_of_memory_data <- function(object)
+{
+    for (x in object) {
+        if (containsOutOfMemoryData(x))
+            return(TRUE)
+    }
+    FALSE
+}
+
+.environment_contains_out_of_memory_data <- function(object)
+{
+    for (name in names(object)) {
+        if (containsOutOfMemoryData(object[[name]]))
+            return(TRUE)
+    }
+    FALSE
+}
+
+setMethod("containsOutOfMemoryData", "ANY",
+    function(object)
+    {
+        if (isS4(object))
+            return(.S4_object_contains_out_of_memory_data(object))
+        FALSE
+    }
+)
+
+setMethod("containsOutOfMemoryData", "list",
+    .list_contains_out_of_memory_data
+)
+
+setMethod("containsOutOfMemoryData", "environment",
+    .environment_contains_out_of_memory_data
+)
 
 setClass("OutOfMemoryObject", representation("VIRTUAL"))
 
